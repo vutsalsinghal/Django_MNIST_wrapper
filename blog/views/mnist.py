e8b89160f443dbd6d0619e3c6b035b9728cc6c2d
Vutsal Singhal <vutsalsinghal@gmail.com>
2017-06-30 00:23:23 +0530

    Accuracy improvements

--- blog/views/mnist.py
+++ blog/views/mnist.py
@@ -2,27 +2,27 @@ from django.http import JsonResponse#, HttpResponse, Http404
 from django.shortcuts import render
 from scipy.misc import imread, imresize#, imsave
 from keras.models import load_model
-import base64
+from io import BytesIO
 import numpy as np
+import base64
+import cv2
 import re
-
-def convertImage(imgData1):
-	imgstr = re.search(r'base64,(.*)',imgData1).group(1)
-	with open('output.png','wb') as output:
-		output.write(base64.b64decode(imgstr))
 
 def page(request):
 	if request.POST:
-		#print('[INFO] Loding saved model and weights from disk...')
-		model = load_model('blog/NN_model/mnist_model.h5')
-		#model.load_weights('blog/NN_model/mnist_weights.h5')
+		model = load_model('/path/to/model/mnist_model.h5')
+		#model.load_weights('/path/to/weights/mnist_weights.h5')
 
-		convertImage(request.POST['img'])
-		x        = imread('output.png',mode='L')
-		x        = np.invert(x)
-		x        = imresize(x,(28,28))
-		#imsave('output1.png', x)
-		out      = model.predict(x.reshape((1,28,28,1)))
+		im = imread(BytesIO(base64.b64decode(re.search(r'base64,(.*)',request.POST['img']).group(1))),mode='L')
+		im = np.invert(im)
+
+		_, contours, hierarchy = cv2.findContours(cv2.threshold(im,127,255,0)[1],cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+		x,y,w,h                = cv2.boundingRect(contours[0])
+		im                     = imresize(im[y:y+h,x:x+w],(28,28))
+
+		#imsave('output_resized.png', im)
+
+		out      = model.predict(im.reshape((1,28,28,1)))
 		response = np.array_str(np.argmax(out,axis=1))
 		return JsonResponse({'response':response})
 	return render(request, 'mnist.html')
